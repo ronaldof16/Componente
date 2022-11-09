2193bd8b65e5a6c693110507e9e00f9176c34171
Ronaldo Fernando <ronaldof16@hotmail.com>
2022-11-09 10:39:23 -0300

    Adição da tag span à tag h2 da segunda section e a criação do método forEach no script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let obrigado = document.querySelector('.agradecimento');
+let telaFinal = document.querySelector('.agradecimento');
 let submit = document.querySelector('.enviar');
 let telaInicial = document.querySelector('.principal');
 let nota = document.querySelectorAll('.nota');
+const notaSelecionada = document.querySelector('#nota');
 
 submit.addEventListener('click', function() {
     telaInicial.style.display = 'none';
-    obrigado.style.display = 'block';
+    telaFinal.style.display = 'block';
+});
+
+nota.forEach((notaClicada) => {
+    notaClicada.addEventListener('click', () => {
+        notaSelecionada.innerHTML = notaClicada.value;
+    });
 });
 
 
